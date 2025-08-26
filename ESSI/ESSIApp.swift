7535b3187a1e2732d39bc4eebedd422df408fcf4
Bob Witmer <bwitmer@golden.net>
2025-08-26 13:13:11 -0400

    Initial Commit

---
+++ ESSI/ESSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESSIApp.swift
+//  ESSI
+//
+//  Created by Bob Witmer on 2025-08-26.
+//
+
+import SwiftUI
+
+@main
+struct ESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
